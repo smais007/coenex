7c3cd82927187ff41074bcc1d97662dadd9820e5
Smais Shawon <smaisshawon@gmail.com>
2025-09-26 21:54:01 +0600

    add landing page layout

--- src/app/(external)/home/layout.tsx
+++ src/app/(external)/home/layout.tsx
@@ -4,7 +4,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div>
       <Navbar />
-      <main className="xs:pt-20 pt-16 sm:pt-24">{children}</main>
+      <main className="xs:pt-20 container mx-auto px-4 pt-16 sm:px-6 sm:pt-24 lg:px-8">{children}</main>
     </div>
   );
 }
